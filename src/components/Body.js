820875382a6f989a65a294eb1c1ea3740709a8a0
srinivas_chamanthi <schamanthi@innominds.com>
2023-08-06 21:13:05 +0530

    ep06/2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,60 @@
 import RestaurantCard from "./RestaurantCard";
-import {resList,newresList} from "../utils/mockData";
+import { resList, newresList } from "../utils/mockData";
 import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(newresList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // const fetchData = async () => {
-  //   const data = await fetch(
-  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.428934&lng=78.3529326&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  //   );
-
-  //   const json = await data.json();
-  //   console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  //   // Optional Chaining
-  //   // setListOfRestaurants(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants);
-  //   // setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-  // };
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.989065&lng=82.247467&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+// 17.440081, 78.348915. gachibowli lat&lan
+// 16.989065, 82.247467 kakinada lat&lan
+    const json = await data.json();
+    console.log(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    // Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    // setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };
 
   return (
     <div className="body">
       <div>
-        <button
+        <button className="top-rated-button"
           style={{
             margin: "10px",
+            padding: "10px",
+            borderRadius: "10px",
+            border: "1px solid #d4d5d9",
             cursor: "pointer",
+            backgroundColor: "#fff",
+            color: "#60b246",
+            fontWeight:"600",
+            fontSize:".9rem",
+            textAlign:"center"
+            
           }}
           onClick={() => {
             setListOfRestaurants(
-              listOfRestaurants.filter((res) => res.info.avgRating > 4)
+              listOfRestaurants.filter((res) => res.info.avgRating >= 4)
             );
           }}
         >
-          Top Rated Restaurants
+          Top Rated
+          <i className="fa fa-star"></i>
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {listOfRestaurants?.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,27 +3,26 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData?.info;
-
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, locality } =
+    resData?.info;
+    const buttonStyle = {
+      backgroundColor: avgRating >= 4 ? '#48c479' : '#db7c38',
+    };
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="res-card">
       <img
         className="res-logo"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
       />
       <h3>{name}</h3>
-      <h4>{cuisines.join(",")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{costForTwo}</h4>
-      {/* <h4>{deliveryTime} minutes</h4> */}
+      <div className="cuisines">{cuisines.join(", ")}</div>
+      <div className="res-card-rating" style={buttonStyle}>
+          <i className="fa fa-star"></i>
+        <span>{avgRating}</span>
+      </div>
+      <h4 className="costForTwo">{costForTwo}</h4>
+      <h4>{locality}</h4>
     </div>
   );
 };
