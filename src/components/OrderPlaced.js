c273d7ea8e23fae2859f3d845557f9be52a1d21c
srinivas_chamanthi <schamanthi@innominds.com>
2023-09-03 16:13:31 +0530

    payment toast

---
+++ src/components/OrderPlaced.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
+import Fab from '@mui/material/Fab';
+import CheckIcon from '@mui/icons-material/Check';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import Typography from '@mui/material/Typography';
+
+export default function OrderPlaced() {
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef();
+
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      '&:hover': {
+        bgcolor: green[700],
+      },
+    }),
+  };
+
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+      handleButtonClick();
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 3000);
+    }
+  };
+
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Fab aria-label="save" color="primary" sx={buttonSx}>
+          {success ? <CheckIcon /> : <FastfoodIcon />}
+        </Fab>
+        {loading && (
+          <>
+            <CircularProgress
+              size={68}
+              sx={{
+                color: green[500],
+                position: 'absolute',
+                top: -6,
+                left: -6,
+                zIndex: 1,
+              }}
+            />
+            <Typography variant="body1" sx={{ zIndex: 2 }}>
+              Order placed successfully
+            </Typography>
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+}
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { clearCart } from "../utils/cartSlice";
+import { useDispatch } from "react-redux";
 
 
 const Payment = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
 
   function itemsTotal() {
     let sum = 0;
@@ -20,9 +24,7 @@ const Payment = () => {
     return sum;
   }
   let itemTotal = itemsTotal();
-  let finalBill = itemTotal + 59 + Math.round((itemTotal * 12) / 100)
-
- 
+  let finalBill = itemTotal + 59 + Math.round((itemTotal * 12) / 100);
 
   const openCheckout = () => {
     const options = {
@@ -32,7 +34,13 @@ const Payment = () => {
       description: "Purchase Description",
       image: "/your_logo.png",
       handler: function (response) {
-        alert(response.razorpay_payment_id);
+        toast.success(
+          `Order placed successfully! Your order ID is ${response.razorpay_payment_id}`,
+          {
+            autoClose: 10000, // 10000 milliseconds = 10 seconds
+          }
+        );
+        dispatch(clearCart());
       },
       prefill: {
         name: "Srinivas Chamanthi",
@@ -53,25 +61,26 @@ const Payment = () => {
   return (
     <div className="billdetails">
       <h2>Bill details</h2>
-      <div style={{display:"flex",justifyContent:"space-between"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3>Item Total</h3>
         <h3>₹{itemTotal}</h3>
       </div>
-      <div style={{display:"flex",justifyContent:"space-between"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3>Delivery Charges</h3>
         <h3>₹59</h3>
       </div>
-      <div style={{display:"flex",justifyContent:"space-between"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3>GST and Restaurant Charges</h3>
         <h3>₹{Math.round((itemTotal * 12) / 100)}</h3>
       </div>
       <div className="_1_jO5"></div>
-      <div style={{display:"flex",justifyContent:"space-between"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3>To Pay</h3>
         <h3>₹{finalBill}</h3>
       </div>
       <div className="billpaybutton" onClick={openCheckout}>
-        Pay Rs. {finalBill}</div>
+        Pay Rs. {finalBill}
+      </div>
     </div>
   );
 };
